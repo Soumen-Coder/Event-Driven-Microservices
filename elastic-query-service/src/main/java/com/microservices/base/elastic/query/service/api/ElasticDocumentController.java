93de7bbeae0d3bd336c6e434e6970fea7ade0060
s0umen <soumen.raj3@gmail.com>
2024-01-08 11:56:54 +0530

    Added comments and springdoc-openapi-ui support and configured the methods of controller with swagger annotations to generate documentations

--- elastic-query-service/src/main/java/com/microservices/base/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/base/elastic/query/service/api/ElasticDocumentController.java
@@ -4,15 +4,18 @@ import com.microservices.base.elastic.query.service.business.ElasticQueryService
 import com.microservices.base.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.microservices.base.elastic.query.service.model.ElasticQueryServiceResponseModel;
 import com.microservices.base.elastic.query.service.model.ElasticQueryServiceResponseModelV2;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController //mixture of @Controller and @ResponseBody
@@ -20,6 +23,7 @@ import java.util.List;
 //@RequestBody is used to deserialize json in httpRequest to an object of ElasticQueryServiceRequestModel automatically
 //@Valid -> checks for validity of attributes inside the bean object, if any of the attribute is non-null or not empty, it validates the entire request object with that.
             //Process validations for all validation annotations on the bean object.
+//Adding media-types on the class level, because it can be easily overridden in the methods
 @RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json") //JSON API with custom vendor media type
 public class ElasticDocumentController {
     private static final Logger LOG = LoggerFactory.getLogger(ElasticDocumentController.class);
@@ -29,13 +33,34 @@ public class ElasticDocumentController {
         this.queryService = queryService;
     }
 
-    @GetMapping("/v1")
+    //Spring-docs -> Swagger Annotations below
+    @Operation(summary = "Get all elastic documents.") //Sets the general description of the methods using summary
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful response.", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class) //Response Model Schema for different response type status code
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @GetMapping("")
     public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getAllDocuments(){
         List<ElasticQueryServiceResponseModel> response = queryService.getAllDocuments();
         LOG.info("Elasticsearch returned {} no of documents ", response.size());
         return ResponseEntity.ok(response); //ok method will set HttpStatusCode as "200"
     }
 
+    @Operation(summary = "Get elastic document by id.") //Sets the general description of the methods using summary
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful response.", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class) //Response Model Schema for different response type status code
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping("/{id}")
     public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModel> getDocumentById(@PathVariable("id") @NotEmpty String id){
         //Mock Object -> ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = ElasticQueryServiceRequestModel.builder().id(id).build();
@@ -44,10 +69,19 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(elasticQueryServiceResponseModel);
     }
 
-    //Additional level of complexity for maintaining the version changes because of a major breaking change in ElasticQueryServiceRequestModel
-    //we had to create a new version, because we couldn't be forward compatible
+    //we had to create a new version, because we couldn't be forward compatible here
     //This change has to be included both in the docs and the api definitions
-    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json") //note the url remains the same, for switching from v1 to v2, client will just change the accept header and not the url
+    @Operation(summary = "Get elastic document by id.") //Sets the general description of the methods using summary
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful response.", content = {
+                    @Content(mediaType = "application/vnd.api.v2+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModelV2.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json") //note the url remains the same, other methods still serving the same old v1(see the produces in the class level, overridden by the methods of the class) url, so here we partially updated a single method to a new version for switching from v1 to v2, client will just change the accept header and not the url
     public @ResponseBody ResponseEntity<ElasticQueryServiceResponseModelV2> getDocumentByIdV2(@PathVariable("id") @NotEmpty String id){
         ElasticQueryServiceResponseModel elasticQueryServiceResponseModel = queryService.getDocumentById(id);
         LOG.info("Elasticsearch returned document with id {} ", id);
@@ -55,6 +89,16 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(elasticQueryServiceResponseModelV2);
     }
 
+    @Operation(summary = "Get elastic documents by text.") //Description by using summary
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful response.", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponseModel.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", description = "Not Found."),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @PostMapping("/get-document-by-text")
     //@Valid annotation -> process the annotations like @NotEmpty, NonNull, etc on the request objects
     public @ResponseBody ResponseEntity<List<ElasticQueryServiceResponseModel>> getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequestModel elasticQueryServiceRequestModel){
@@ -66,6 +110,7 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(response);
     }
 
+    //Mapping of ElasticQueryServiceResponseModel to the new ElasticQueryServiceResponseModelV2 setting the new field as well and parsing the id to Long from String
     private ElasticQueryServiceResponseModelV2 getV2Model(ElasticQueryServiceResponseModel responseModel){
         ElasticQueryServiceResponseModelV2 responseModelV2 = ElasticQueryServiceResponseModelV2.builder()
                 .id(Long.parseLong(responseModel.getId()))
@@ -77,3 +122,12 @@ public class ElasticDocumentController {
         return responseModelV2;
     }
 }
+
+//OpenAPI Specification -> (OAS) defines a standard, language-agnostic interface to the RESTful APIs which allows both humans and computers to discover
+//and understand the capabilities of the service, without access to code, docs or through network traffic inspection.
+
+//Spring docs adds the OpenAPI Specification to a Spring Boot Application comes with a single dependency -> springdoc-openapi-ui
+//Uses Swagger3 as the implementation
+//By including the above dependency, it automates the generation of the API documentation(via various swagger annotations like @ApiResponse, @Operation)
+//It supports various formats like JSON, YAML and HTML formats.
+//Finally thanks to Swagger3, Spring doc also supports Content Negotiation(Accept Headers) Versioning(application/vnd.api.v1+json) which was not possible with swagger2 version
--- elastic-query-service/src/main/java/com/microservices/base/elastic/query/service/model/ElasticQueryServiceResponseModel.java
+++ elastic-query-service/src/main/java/com/microservices/base/elastic/query/service/model/ElasticQueryServiceResponseModel.java
@@ -19,11 +19,12 @@ public class ElasticQueryServiceResponseModel extends RepresentationModel<Elasti
     private String id;
     private Long userId;
     private String text;
-    private String textV2; // think if this is a breaking change? actually no, because of the extensible nature of json, by adding a field, we are okay on the client side and deserialization will be done correctly
+    private String textV2; // think if this is a breaking change? actually no, because of the extensible nature of json, by adding a new field, we are okay on the client side and deserialization will be done correctly, only it will not include the new field "textV2"
     //if any client need this field, then client can change their code, to include this field during deserialization process
     //This change doesn't require a new creation of a version
     //We will deploy the changes and mention them in the documentation
     //This is enough safe and easy for both server and client
+
     //Now let's say we want to change the type of id field to Long and also remove the createdAt field, then it will be breaking change and we have to create a new version
     private LocalDateTime createdAt;
 }
